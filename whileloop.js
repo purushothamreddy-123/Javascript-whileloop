922c7d66cd927ce25c9d506c130d744c11bf10e1
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-17 15:06:32 +0530

    Javascript whileloop

---
+++ whileloop.js
@@ -0,0 +1,16 @@
+let i = 1, n = 5;
+
+while (i <= n) {
+    console.log(i);
+    i += 1;
+}
+
+
+// do while
+// let i = 1;
+// const n = 5;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= n);
